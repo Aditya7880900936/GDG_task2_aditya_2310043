b345b69ec364b5ddca50160e26b39e16d4b29671
vanshguptaji <vansh23164033@akgec.ac.in>
2024-11-03 13:33:17 +0530

    Working on the state managment system and saving of local storage data items

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter , Navigate , Routes , Route} from 'react-router-dom'
+import HomePage from './scenes/homePage';
+import login from './scenes/loginPage/login';
+import profile from './scenes/profilePage/profile';
+import navbar from './scenes/navbar/nav';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <BrowserRouter>
+          <Routes>
+            <Route path='/' element = {<login/>}/>
+            <Route path='/home' element = {<HomePage/>} />
+            <Route path='/profile/:userId' element = {<profile/>} />
+          </Routes>
+       </BrowserRouter>
     </div>
   );
 }
